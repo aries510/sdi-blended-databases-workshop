213c27231dab71d5fcde31801c2fbc2d1e752c23
aries510 <darenrespicio@gmail.com>
2026-08-19 15:33:17 -0600

    added foreign keys for engine, transmission, and drivetrain

--- migrations/20260819192353_create_car_models_table.js
+++ migrations/20260819192353_create_car_models_table.js
@@ -6,13 +6,18 @@ exports.up = function(knex) {
   return knex.schema
     .createTable('car_models', function(table) {
         table.increments('id');
-        table.string('manufacturer_id').unsigned();
-        table.foreign('manufacturer_id').references('car_manufacturers.id')
+        table.string('manufacturer_id');
+        table.foreign('manufacturer_id').references('car_manufacturers.id');
         table.string('name').notNullable();
+        table.string('chassis_code');
+        table.string('years_produced')
         table.string('engine');
+        table.foreign('engine').references('engine_types.id')
         table.decimal('engine_size_in_liters')
         table.string('transmission');
+        table.foreign('transmission').references('transmission_types.id');
         table.string('drivetrain');
+        table.foreign('drivertrain').references('drivetrain_types.id')
         table.integer('hp');
     });
 };
